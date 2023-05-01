620e99bc7e578a4553588f5bd02f818da5ab12bc
Andras Serfozo <subztep@gmail.com>
2023-05-01 06:46:04 +0100

    Server origin

--- src/server.ts
+++ src/server.ts
@@ -11,59 +11,59 @@ const port = Number(process.env.PORT)
 const app = express()
 app.use(express.static(join(__dirname, "/public")))
 
-const server = createServer(app)
-const wss = new WebSocketServer({ server })
+// const server = createServer(app)
+// const wss = new WebSocketServer({ server })
 
-wss.on("connection", function (ws) {
-  const id = setInterval(function () {
-    ws.send(JSON.stringify(process.memoryUsage()), function () {
-      //
-      // Ignoring errors.
-      //
-    })
-  }, 100)
-  console.log("started client interval")
+// wss.on("connection", function (ws) {
+//   const id = setInterval(function () {
+//     ws.send(JSON.stringify(process.memoryUsage()), function () {
+//       //
+//       // Ignoring errors.
+//       //
+//     })
+//   }, 100)
+//   console.log("started client interval")
 
-  ws.on("close", function () {
-    console.log("stopping client interval")
-    clearInterval(id)
-  })
-})
+//   ws.on("close", function () {
+//     console.log("stopping client interval")
+//     clearInterval(id)
+//   })
+// })
 
-server.listen(port, function () {
-  console.log(`Listening on http://0.0.0.0:${port}`)
-})
+// server.listen(port, function () {
+//   console.log(`Listening on http://0.0.0.0:${port}`)
+// })
 
 // import express from "express"
 // import WebSocket, { WebSocketServer } from "ws"
 
 // const port = Number(process.env.PORT)
 
-// const wss = new WebSocketServer(
-//   {
-//     port,
-//     clientTracking: true,
-//   },
-//   () => {
-//     console.log("WebSocket server running on port", port)
-//   }
-// )
+const wss = new WebSocketServer(
+  {
+    port,
+    clientTracking: true,
+  },
+  () => {
+    console.log("WebSocket server running on port", port)
+  }
+)
 
-// wss.on("connection", (ws) => {
-//   console.log("Connection")
+wss.on("connection", (ws) => {
+  console.log("Connection")
 
-//   ws.on("error", (ev) => console.log("WS Error", ev))
+  ws.on("error", (ev) => console.log("WS Error", ev))
 
-//   ws.on("message", (data, binary) => {
-//     console.log("Message", String(data))
-//     wss.clients.forEach((client) => {
-//       if (client.readyState === WebSocket.OPEN) {
-//         client.send(`re:${data}`, { binary })
-//       }
-//     })
-//   })
-// })
+  ws.on("message", (data, binary) => {
+    console.log("Message", String(data))
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(`re:${data}`, { binary })
+      }
+    })
+  })
+})
 
-// wss.on("error", (ev) => console.log("WSS Error", ev))
+wss.on("error", (ev) => console.log("WSS Error", ev))
 
 // export {}
