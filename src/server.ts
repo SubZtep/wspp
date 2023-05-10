b95b59c1069ab702b7ae30249da6a99bab96380b
Andras Serfozo <subztep@gmail.com>
2023-05-11 00:14:59 +0100

    Cleanup

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,6 @@ import { createServer } from "node:http"
 import express from "express"
 import WebSocket, { WebSocketServer } from "ws"
 
-/** Sessions */
-// const ids = new Map<string, any>()
-
 const app = express()
 app.set("views", resolve(dirname(fileURLToPath(import.meta.url)), "../views"))
 app.set("view engine", "pug")
@@ -21,36 +18,17 @@ const wss = new WebSocketServer({ server })
 
 wss.on("connection", ws => {
   console.log("Connection")
-  // if (ids.has(ws)) {
 
   ws.on("error", ev => console.log("WS Error", ev))
 
   ws.on("message", (data, binary) => {
-    // const msg = JSON.parse(String(data))
-    // if (!ids.has(msg.id)) {
-    //   ids.set(msg.id, msg)
-    //   // const client = ids.get(msg.id)
-    //   // client.send(data, { binary })
-    // }
-
-    // console.log("Message", String(data))
-
     wss.clients.forEach(client => {
       // if (client !== ws && client.readyState === WebSocket.OPEN) {
       if (client.readyState === WebSocket.OPEN) {
         client.send(data, { binary })
-        // client.send(`re:${data}`, { binary })
       }
     })
   })
-
-  // const id = setInterval(() => {
-  //   ws.send(JSON.stringify(process.memoryUsage()), err => {
-  //     if (err) {
-  //       console.log("Error sending message", err)
-  //     }
-  //   })
-  // }, 1_000)
 
   ws.on("upgrade", () => {
     console.log("WS Upgrade")
@@ -58,8 +36,6 @@ wss.on("connection", ws => {
 
   ws.on("close", () => {
     console.log("WS Close")
-    // console.log("stopping client interval")
-    // clearInterval(id)
   })
 })
 
